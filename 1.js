a8a9965e18a915128c5d9ffca70c6da2030f9359
Mike <CliqXT@ambrosialpariah.com>
2011-07-19 13:51:52 -0400

    second commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+{
+    "developer": "AmbrosialROMs",
+    "free": "true",
+    "id": "RealPariah",
+    "icon": "http://www.ambrosialpariah.com/ambrosialcm7/images/rm.logo.png",
+    "manifest": "http://www.ambrosialpariah.com/ambrosialcm7/files/rom.man/acm.js",
+    "roms": {
+        "zeppelin": "true"
+    },
+    "summary": "ambrosialROMs: Adding the sizzle to the Bacon!"
+}
---
+++ acm.js
@@ -0,0 +1,14 @@
+{
+    "version": "1",
+    "homepage": "http://www.ambrosialpariah.com/ambrosialcm7/",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Y8WZZNXKPNXA6",
+
+    "ROMs":
+    [
+    {
+        "name": "AmbrosialCM7 v3.0.5",
+        "summary": "Stable",
+        "device": "zeppelin",
+        "url": "http://www.ambrosialpariah.com/ambrosialcm7/files/AmbrosialCM7-v3.0.5-n133.loaded.signed.zip"
+    },
+}
