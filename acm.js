4ac0a8b8dc1a365c06943ffcd3273ad96bdeec70
Mike <CliqXT@ambrosialpariah.com>
2011-07-21 05:14:17 -0700

    edited via git hub - First Working version of my own manifest!

--- acm.js
+++ acm.js
@@ -2,13 +2,21 @@
     "version": "1",
     "homepage": "http://www.ambrosialpariah.com/ambrosialcm7/",
     "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Y8WZZNXKPNXA6",
-
-    "ROMs":
-    [
-    {
-        "name": "AmbrosialCM7 v3.0.5",
-        "summary": "Stable",
-        "device": "zeppelin",
-        "url": "http://www.ambrosialpariah.com/ambrosialcm7/files/AmbrosialCM7-v3.0.5-n133.loaded.signed.zip"
-    },
+    "roms": [
+        {
+            "device": "zeppelin",
+            "name": "AmbrosialCM7 3.0.5",
+            "modversion": "AmbrosialCM7-3.0.5-CliqXT",
+            "incremental": "3050",
+            "url": "http://www.ambrosialpariah.com/ambrosialcm7/files/AmbrosialCM7-v3.0.5-n133.loaded.signed.zip",
+            "product": "AmbrosialCM7",
+            "summary": "Stable Release",
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/gapps/gapps-gb-20110613-signed.zip"
+                }
+            ]
+        }
+    ]
 }
